91eac17872996f1a4e600fe64d1c17ff6bfdbe19
Adailton Moura <adailton@agilytecnologia.com.br>
2020-12-13 13:27:05 -0300

    Mudando a Request do laboratório.

--- app/Http/Controllers/LabsController.php
+++ app/Http/Controllers/LabsController.php
@@ -39,7 +39,7 @@ class LabsController extends Controller
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(LabsRequest $request)
+    public function store(Request $request)
     {
         $this->authorize('isAdmin');
         $request->validate([
