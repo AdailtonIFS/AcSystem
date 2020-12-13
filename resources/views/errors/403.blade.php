3fccd877956bd7015f022507ce96e1e1f8af8371
Adailton Moura <adailton@agilytecnologia.com.br>
2020-12-13 14:10:57 -0300

    Mudando mensagem de permissão.

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
-@section('title', __('Forbidden'))
+@section('title', __('Não autorizado'))
 @section('code', '403')
-@section('message', __($exception->getMessage() ?: 'Forbidden'))
+@section('message', __('Você não tem autorização para realizar essa ação' ?: 'Forbidden'))
