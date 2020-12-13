365391c60ced75916b7e70579287b589e536f00b
Adailton Moura <adailton@agilytecnologia.com.br>
2020-12-13 14:06:54 -0300

    Corrigindo erro na rota de ocorrencias nos laboratórios e colocando permissão no metodo show e edit

--- app/Http/Controllers/OccurrencesController.php
+++ app/Http/Controllers/OccurrencesController.php
@@ -108,7 +108,7 @@ class OccurrencesController extends Controller
 
     public function edit(Occurrence $occurrence)
     {
-        $this->authorize('update-occurrence', [$occurrence]); a
+        $this->authorize('update-occurrence', [$occurrence]);
         try {
             $laboratories = Laboratory::all();
             $user = $occurrence->user()->first();
