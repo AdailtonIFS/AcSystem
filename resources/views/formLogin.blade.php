b88f7b6d83ab4f5260125c93f04e1199297b1253
Adailton Moura <adailton@agilytecnologia.com.br>
2020-12-13 02:45:23 -0300

    Mudando bootstrap pra CDN, teste.

--- resources/views/formLogin.blade.php
+++ resources/views/formLogin.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ url(mix('/css/bootstrap.css')) }}">
-    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    <title>Login</title>
 </head>
 <body class="h-100">
     <div class="container pt-5">
