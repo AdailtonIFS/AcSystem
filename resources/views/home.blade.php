fe8187fcda7a187a18ba04fe4c21bd8a5ce24f36
Adailton Moura <adailton@agilytecnologia.com.br>
2020-12-16 08:12:55 -0300

    mudando home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
     <div class="container h-25  d-flex justify-content-center align-items-center position-absolute">
         @if(now()->format('H') >= 5 && now()->format('i') > 1 && now()->format('H') <= 12)
             <h2 class="mb-5">
-                {{'Bom dia' .auth()->user()->name}}
+                {{'Bom dia ' .auth()->user()->name}}
             </h2>
 
         @endif
@@ -49,18 +49,9 @@
                     @foreach($occurrences as $occurrence)
                         <tr class="text-center">
                             <td>
-                                <a @can('isAdmin')href="{{route('occurrences.show', ['occurrence' => $occurrence->id])}}@endcan">{{$occurrence->occurrence}}</a>
-                            </td>
-                            <td>
-                                <a @can('isAdmin')href="{{route('users.show', ['user' => $occurrence->user_registration])}}"@endcan>{{$occurrence->user_name}}</a>
+                                <a @can('isAdmin') href="{{route('occurrences.show', ['occurrence' => $occurrence->id])}}@endcan">{{$occurrence->occurrence}}</a>
                             </td>
                             <td>{{$occurrence->date}}</td>
-                            <td>{{$occurrence->hour}}</td>
-                            @can('isAdmin')
-                                <td>
-                                    <a href="{{route('occurrences.edit', ['occurrence' => $occurrence->id])}}">Editar</a>
-                                </td>
-                            @endcan
                         </tr>
                     @endforeach
                 @endif
